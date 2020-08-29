f223c8b71c36236508ddf70107748d21b351b766
Dmitrii Zverev <61390189+Zerrtry@users.noreply.github.com>
2020-08-28 23:37:11 -0500

    Revert "minor changes for testing Travis CI"

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Add routes API
+// Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
