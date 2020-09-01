1578c1c117eea9a1947ec679c57c540300676dac
Dmitrii <zerrtry@icloud.com>
2020-09-01 17:00:45 -0500

    authentification func

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,34 +17,50 @@ module.exports = {
   
   create: async function(req, res) {
     console.log("cretate-func", req.body)
-    const creds = saltHash(req.body.password);
-    const token = createSessiontoken();
-    req.body.password = creds.password;
-    req.body.salt = creds.salt;
-    req.body.sessionToken = token;
-    //!!!!have to create a function for chacking out if th euser name is already exist
-    await db.User.create(req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    console.log("userObj", req.body.userData.userName)
+    let account = await db.User.findOne({ 'userData.userName': req.body.userData.userName});
+    console.log("account",account)
+    if (!account) { 
+      const creds = saltHash(req.body.password);
+      const token = createSessiontoken();
+      req.body.password = creds.password;
+      req.body.salt = creds.salt;
+      req.body.userData.sessionToken = token;
+      await db.User.create(req.body)
+        .then(result => res.json(result.userData))
+        .catch(err => res.status(422).json(err));
+    } else {
+      res.json("User name already taken.")
+    }
   },
   
   authenticate: async function(req, res) {
     console.log('reqest', req.params.id1)
-    const userObj = {
-      userName: req.params.id1
-    }; 
     const password = req.params.id2;
-    const account = await db.User.findOne(userObj);
-    const passwordHash = account.password;
-    const match = await bcrypt.compare(password, passwordHash);
- 
-    if(match) {
-      //login 
-      res.json("User identeficated.")
+    let account = await db.User
+      .findOne({ 'userData.userName': req.params.id1});
+    console.log("account",account)
+    if (account) { 
+      const passwordHash = account.password;
+      let match = await bcrypt.compare(password, passwordHash);
+      console.log("match",match)
+      if(match) {
+        const token = createSessiontoken()
+        console.log("token",token)
+        console.log("username", account.userData.userName);
+        await db.User.findOneAndUpdate(
+          {'userData.userName': req.params.id1},
+          {'userData.sessionToken': token},
+          {new: true}
+        )
+        .then(result => res.json(result.userData))
+      } else {
+        res.json("Wrong password.")
+      }
     } else {
-      res.json("Wrong credantials.")
+      res.json("User not found.")
     }
-   } 
+  } 
 
   // findAll: function(req, res) {
       //   db.Post.find(req.query)
