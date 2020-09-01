0326730c021ca585223f1399fe3723e5d726d0f9
Dmitrii <zerrtry@icloud.com>
2020-08-31 23:25:43 -0500

    authentication brunch added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,26 +1,59 @@
 const db = require("../models");
+const bcrypt = require('bcrypt');
+
+const createSessiontoken = () => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+const saltHash = (pass) => {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(pass, salt);
+    console.log("saltHash", salt, password)
+    return {password, salt};
+}
 
 // Defining methods for the postsController
 module.exports = {
-  // findAll: function(req, res) {
-  //   db.Post.find(req.query)
-  //     .sort({ date: -1 })
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  findByUserName: function(req, res) {
-    const nameObj = {userName: req.params.id}
-    db.User.find(nameObj)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  
+  create: async function(req, res) {
+    console.log("cretate-func", req.body)
+    const creds = saltHash(req.body.password);
+    const token = createSessiontoken();
+    req.body.password = creds.password;
+    req.body.salt = creds.salt;
+    req.body.sessionToken = token;
+    //!!!!have to create a function for chacking out if th euser name is already exist
+    await db.User.create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.User.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+  
+  authenticate: async function(req, res) {
+    console.log('reqest', req.params.id1)
+    const userObj = {
+      userName: req.params.id1
+    }; 
+    const password = req.params.id2;
+    const account = await db.User.findOne(userObj);
+    const passwordHash = account.password;
+    const match = await bcrypt.compare(password, passwordHash);
+ 
+    if(match) {
+      //login 
+      res.json("User identeficated.")
+    } else {
+      res.json("Wrong credantials.")
+    }
+   } 
+
+  // findAll: function(req, res) {
+      //   db.Post.find(req.query)
+      //     .sort({ date: -1 })
+      //     .then(dbModel => res.json(dbModel))
+      //     .catch(err => res.status(422).json(err));
+      // },
   // update: function(req, res) {
-  //   db.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
+    //   db.Post.findOneAndUpdate({ _id: req.params.id }, req.body)
   //     .then(dbModel => res.json(dbModel))
   //     .catch(err => res.status(422).json(err));
   // },
